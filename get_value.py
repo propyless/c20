37843202f37ba80e3a98a3c2a1f2a38f19b47de5
Takeshi <6172961+propyless@users.noreply.github.com>
2017-12-07 18:18:00 +0100

    Make USD as currency default

--- get_value.py
+++ get_value.py
@@ -7,17 +7,17 @@ from pathlib import Path
 home = str(Path.home())
 config = load_config(f'{home}/.config/c20.yaml')
 num_tokens = config.get('num_tokens')
-currency = config.get('currency', 'SEK')
-
-
+currency = config.get('currency', 'USD').upper()
+  
 # Get token value
 response = requests.get('https://us-central1-cryptodash1.cloudfunctions.net/fundValue')
 value_per_token = response.json().get('nav_per_token')
 
-if currency.upper() == 'USD':
-  print(int(value_per_token*num_tokens))
+if currency == 'USD':
+  exchange_rate = 1
 else:
   # Get dollar to chosen currency exchange rate
   response = requests.get('https://api.fixer.io/latest?base=USD')
   exchange_rate = response.json().get('rates').get(currency)
-  print(int(value_per_token*num_tokens*exchange_rate))
+ 
+print(int(value_per_token*num_tokens*exchange_rate))
